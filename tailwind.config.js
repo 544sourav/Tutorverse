fb8daef63c6c6effaca1ca56f603b732ca3b9167
544sourav <142586690+544sourav@users.noreply.github.com>
2023-10-09 14:34:43 +0530

    add

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        grayblack:"#2b2e31",
+        grayblackdeep:"#2a2f30",
+        orange:"#e46f41",
+        deeporange:"#dc7044",
+        grayblue:"#007385"
+      },
+    },
+  },
+  plugins: [],
+}
+
